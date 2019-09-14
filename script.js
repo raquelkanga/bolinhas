0aea2c082215213f1aa08c1f2d3a9f6417356a7d
raquel <raquelkanga@gmail.com>
2019-09-14 13:54:26 -0300

    Finalizando exercício das bolinhas

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+   $(".red").click(function(){
+       $(this).fadeOut();
+   });
+    
+   $(".green").click(function(){
+      $(".blue").hide(); 
+   });
+    
+   $(".yellow").click(function(){
+      $(".blue").fadeIn();
+      $(".green").hide();   
+   });
+    
+   $(".blue").click(function(){
+      $(".green, .red, .yellow").toggle(); 
+   });
+    
+    
+});
